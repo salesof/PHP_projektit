efd1a9dc117caa3ea4d5f8f81a47332a088330eb
salesof <sofia.salejarvi@gmail.com>
2024-09-18 16:37:59 +0300

    DEBUG definition

--- kukkakauppa/asetukset.php
+++ kukkakauppa/asetukset.php
@@ -15,6 +15,7 @@ define("EMAIL_FROM_NAME", "Puutarhaliike Neilikka");
 $DB = "neilikka";
 $LOCAL = in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'REMOTE_ADDR' => '::1'));
 if ($LOCAL) {
+    define("DEBUG", true);
     $tunnukset = "tunnukset.php";
     if (file_exists($tunnukset)) {
         include_once("tunnukset.php");
@@ -26,6 +27,7 @@ if ($LOCAL) {
     $db_password = $db_password_local;
     $EMAIL_ADMIN = $admin_mail;
 } elseif (strpos($_SERVER['HTTP_HOST'], "azurewebsites") !== false) {
+    define("DEBUG", false);
     $db_server = $_ENV['MYSQL_HOSTNAME'] ?? getenv('MYSQL_HOSTNAME');
     $db_username = $_ENV['MYSQL_USERNAME'] ?? getenv('MYSQL_USERNAME');
     $db_password = $_ENV['MYSQL_PASSWORD'] ?? getenv('MYSQL_PASSWORD');
