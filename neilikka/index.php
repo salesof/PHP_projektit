e4d24bd8a6fd9cfe4bd9c98b7fbebbc2df3d2751
salesof <sofia.salejarvi@gmail.com>
2024-09-20 15:36:43 +0300

    added database sql statements

--- neilikka/index.php
+++ neilikka/index.php
@@ -1,39 +1,40 @@
 <!DOCTYPE html>
 <html>
 <?php include "modules/head.php" ?>
-  <body>
+
+<body>
   <?php include "modules/nav.php" ?>
   <?php include "modules/banner.php" ?>
 
-    <div class="container">
-      <h1>Etusivu</h1>
-      <p>
-        Tervetuloa Puutarhaliike Neilikan kotisivuille! Meiltä löydät sekä sisä-
-        että ulkokasvit ja kaiken tarvitsemasi kasvien hoitoon.
-      </p>
+  <div class="container">
+    <h1>Etusivu</h1>
+    <p>
+      Tervetuloa Puutarhaliike Neilikan kotisivuille! Meiltä löydät sekä sisä-
+      että ulkokasvit ja kaiken tarvitsemasi kasvien hoitoon.
+    </p>
 
-      <p>
-        Olipa toiveissasi vehreä viidakko sisätiloihin, värikäs kukkaloisto
-        parvekkeelle tai upea piha- ja puutarhakokonaisuus, Puutarhaliike
-        Neilikka auttaa sinua toteuttamaan unelmasi. Asiantunteva
-        henkilökuntamme on apunasi valitsemassa juuri sinun tarpeisiisi sopivat
-        kasvit ja hoitotuotteet, jotka menestyvät niin kotona kuin ulkona.
-        Laajasta valikoimastamme löydät myös laadukkaat työkalut ja tarvikkeet,
-        jotka tekevät puutarhatöistä vaivattomia ja mukavia.
-      </p>
-      <p>
-        Tutustu rauhassa tuotteisiimme ja palveluihimme verkkosivuillamme tai
-        tule käymään myymälässämme! Tarjoamme inspiraatiota, vinkkejä ja ideoita
-        kaikenlaisille viherpeukaloille. Puutarhaliike Neilikka on täällä
-        auttamassa sinua luomaan omasta ympäristöstäsi kauniin ja viihtyisän
-        keitaan – juuri sellaisen, josta olet aina haaveillut.
-      </p>
+    <p>
+      Olipa toiveissasi vehreä viidakko sisätiloihin, värikäs kukkaloisto
+      parvekkeelle tai upea piha- ja puutarhakokonaisuus, Puutarhaliike
+      Neilikka auttaa sinua toteuttamaan unelmasi. Asiantunteva
+      henkilökuntamme on apunasi valitsemassa juuri sinun tarpeisiisi sopivat
+      kasvit ja hoitotuotteet, jotka menestyvät niin kotona kuin ulkona.
+      Laajasta valikoimastamme löydät myös laadukkaat työkalut ja tarvikkeet,
+      jotka tekevät puutarhatöistä vaivattomia ja mukavia.
+    </p>
+    <p>
+      Tutustu rauhassa tuotteisiimme ja palveluihimme verkkosivuillamme tai
+      tule käymään myymälässämme! Tarjoamme inspiraatiota, vinkkejä ja ideoita
+      kaikenlaisille viherpeukaloille. Puutarhaliike Neilikka on täällä
+      auttamassa sinua luomaan omasta ympäristöstäsi kauniin ja viihtyisän
+      keitaan – juuri sellaisen, josta olet aina haaveillut.
+    </p>
 
-      <h2>Uutisia</h2>
+    <h2>Uutisia</h2>
 
-      <div class="flex-wrapper"></div>
+    <div class="flex-wrapper"></div>
 
-      <!-- Old page content without db.json
+    <!-- Old page content without db.json
       <div class="flex-wrapper">
         <div class="news-block">
           <img src="img/news10.jpg" />
@@ -93,14 +94,15 @@
       </div>
       -->
 
-      <div class="center-block">
-        <a href="uutiset.php"><button>Näytä kaikki uutiset</button></a>
-      </div>
+    <div class="center-block">
+      <a href="uutiset.php"><button>Näytä kaikki uutiset</button></a>
     </div>
+  </div>
 
-    <?php
-      $js = "script/index.js";
-      include "modules/footer.php"
-    ?>
-  </body>
+  <?php
+  $js = "script/index.js";
+  include "modules/footer.php"
+  ?>
+</body>
+
 </html>
--- neilikka/script/index.js
+++ neilikka/script/index.js
@@ -4,55 +4,55 @@ let displayCounter = 0;
 const quantity = 6; // Ladataan 6 postausta
 
 // Kun DOM latautuu, lataa ja lajittele koko aineisto
-document.addEventListener('DOMContentLoaded', loadAndSort);
+document.addEventListener("DOMContentLoaded", loadAndSort);
 
 // Lataa koko aineisto ja lajittele se
 function loadAndSort() {
-    fetch('http://localhost:3001/news')
-    .then(response => response.json())
-    .then(data => {
-        // Lajittele data id:n mukaan käänteiseen järjestykseen
-        allPosts = data.sort((a, b) => b.id - a.id);
-        // Lataa ensimmäiset postaukset
-        loadNextPosts();
+  fetch("http://localhost:3001/news")
+    .then((response) => response.json())
+    .then((data) => {
+      // Lajittele data id:n mukaan käänteiseen järjestykseen
+      allPosts = data.sort((a, b) => b.id - a.id);
+      // Lataa ensimmäiset postaukset
+      loadNextPosts();
     });
 }
 
 // Lataa seuraavat postaukset DOM:iin
 function loadNextPosts() {
-    // Määritä aloitus- ja lopetusnumerot postauksille
-    const start = displayCounter;
-    const end = start + quantity;
+  // Määritä aloitus- ja lopetusnumerot postauksille
+  const start = displayCounter;
+  const end = start + quantity;
 
-    // Määritä, mitkä postaukset ladataan
-    const postsToDisplay = allPosts.slice(start, end);
+  // Määritä, mitkä postaukset ladataan
+  const postsToDisplay = allPosts.slice(start, end);
 
-    // Lisää lajitelut postaukset DOM:iin
-    postsToDisplay.forEach(add_post);
+  // Lisää lajitelut postaukset DOM:iin
+  postsToDisplay.forEach(add_post);
 }
 
 // Lisää uusi postaus DOM:iin
 function add_post(contents) {
-    // Luo uusia divejä sisältöineen
-    const post = document.createElement('div');
-    post.className = 'news-block';
+  // Luo uusia divejä sisältöineen
+  const post = document.createElement("div");
+  post.className = "news-block";
 
-    const image = document.createElement('img');
-    image.src = contents.imgUrl;
-    image.alt = "Posted image";
+  const image = document.createElement("img");
+  image.src = contents.imgUrl;
+  image.alt = "Posted image";
 
-    const date = document.createElement('p');
-    date.className = 'news-date';
-    date.innerHTML = contents.date;
+  const date = document.createElement("p");
+  date.className = "news-date";
+  date.innerHTML = contents.date;
 
-    const text = document.createElement('p');
-    text.innerHTML = contents.excerpt;
+  const text = document.createElement("p");
+  text.innerHTML = contents.excerpt;
 
-    // Koosta luoduista diveistä oikea rakenne
-    post.appendChild(image);
-    post.appendChild(date);
-    post.appendChild(text);
+  // Koosta luoduista diveistä oikea rakenne
+  post.appendChild(image);
+  post.appendChild(date);
+  post.appendChild(text);
 
-    // Lisää postaus DOM:iin
-    document.querySelector('.flex-wrapper').append(post);
+  // Lisää postaus DOM:iin
+  document.querySelector(".flex-wrapper").append(post);
 }
